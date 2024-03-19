5ebaa135bc920660f6b4123acc327cb235a39ecc
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-03-19 19:59:53 +0700

    cout masukan kata selamat puasa

---
+++ 050_LoopingFungsi/050_LoopingFungsi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    string nama[5];
+
+    for (i = 150; i > 100; i -= 10)
+    {
+        cout << i << " . " << "SELAMAT PUASA" << endl;
+
+    }
+    cout << "Nilai i terkahir = " << i << endl;
+
+    for (i = 0; i < 5; i++)
+    {
+        cout << "Masukan nama ke-" << i << ":";
+        cin >> nama[i];
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        cout << "Nama ke-" << i << ":" << nama[i] << endl;
+    }
+//}
